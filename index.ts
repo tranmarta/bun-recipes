320d6c34a566a75964285a8f19402982ed022f8b
Marta <tran.marta@gmail.com>
2023-11-15 21:13:28 +0100

    server setup

--- index.ts
+++ index.ts
@@ -1,1 +1,8 @@
-// Empty starter file
+const server = Bun.serve({
+  port: Bun.env.PORT || 3000,
+  fetch(req) {
+    return new Response("Nice bun!");
+  },
+});
+
+console.log(`Server is running on port ${server.port}`);
